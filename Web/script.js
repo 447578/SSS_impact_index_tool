6ba302b9e7fa28907fac37fd3b3baa1bdb684162
katpein <katpein@users.noreply.github.com>
2020-01-08 16:09:34 +0100

    Jan 08th
    Recovery

---
+++ Web/script.js
@@ -0,0 +1,36 @@
+
+
+var app = angular.module('demo', ['angular-progress-arc']);
+
+app.controller('DemoCtrl', function ($scope) {
+
+  // Init progress value
+  $scope.progress = 0.74;
+});
+
+
+$("document").ready(function() {
+  $(".slider").rangeslider();
+});
+$.fn.rangeslider = function(options) {
+  var obj = this;
+  var defautValue = obj.attr("value");
+  obj.wrap("<span class='range-slider'></span>");
+  obj.after("<span class='slider-container'><span class='bar'><span></span></span><span class='bar-btn'><span>0</span></span></span>");
+  obj.attr("oninput", "updateSlider(this)");
+  updateSlider(this);
+  return obj;
+};
+
+function updateSlider(passObj) {
+  var obj = $(passObj);
+  var value = obj.val();
+  var min = obj.attr("min");
+  var max = obj.attr("max");
+  var range = Math.round(max - min);
+  var percentage = Math.round((value - min) * 100 / range);
+  var nextObj = obj.next();
+  nextObj.find("span.bar-btn").css("left", percentage + "%");
+  nextObj.find("span.bar > span").css("width", percentage + "%");
+  nextObj.find("span.bar-btn > span").text(percentage);
+};
