fbb60c581fca3b3d243fb21f7cbfa60b5d127b2d
katpein <katpein@users.noreply.github.com>
2020-01-08 19:03:24 +0100

    daily update

--- Web/script.js
+++ Web/script.js
@@ -38,3 +38,15 @@ function updateSlider(passObj) {
   nextObj.find("span.bar > span").css("width", percentage + "%");
   nextObj.find("span.bar-btn > span").text(percentage);
 };
+//Active class
+var header = document.getElementById("myList");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) {
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+}
